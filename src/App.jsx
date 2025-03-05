eba1e6baaeba1a5c76918142456679a6d1b0320d
kris <krismwasgit@gmail.com>
2025-03-05 12:31:26 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalSyles";
+
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.main`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Hello world</Heading>
+            <Heading as="h2">Hello world</Heading>
+
+            <Button onClick={() => alert("thi is is")}>Check in</Button>
+            <Button
+              variation="secondary"
+              size="small"
+              onClick={() => alert("thi is is")}
+            >
+              Check in
+            </Button>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Hello world</Heading>
+            <form>
+              <Input type="number" placeholder="number of guests" />
+              <Input type="number" placeholder="number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
